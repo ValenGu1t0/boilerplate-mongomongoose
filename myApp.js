2e481f3d973681f80c1dfa4aeec215da01835d02
ValenGu1t0 <devalen543@gmail.com>
2025-04-13 12:07:26 -0300

    Connect Final

--- myApp.js
+++ myApp.js
@@ -6,9 +6,9 @@
 
 require('dotenv').config();
 
-const mongoose = require('mongoose');
+let mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI;
+let MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
