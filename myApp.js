5ed59428640e9399d29ecf2e724b5a1254a3c8e5
ValenGu1t0 <devalen543@gmail.com>
2025-04-13 11:42:24 -0300

    Connect - 2

--- myApp.js
+++ myApp.js
@@ -1,14 +1,17 @@
-require('dotenv').config();
-
-require('mongoose');
-
 // username - devalen543
 
 // DB user password - dshcgtRam1utIzfc
 
 // fcc-mongodb-and-mongoose - nombre de la DB
 
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const MONGO_URI = process.env.MONGO_URI;
+
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 
 let Person;
